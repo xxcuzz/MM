6fa7c28719e1eae9f0e581738d3a8f6cc4888e07
xxcuzz <xxcuzzx@gmail.com>
2018-07-05 20:03:31 +0300

    add button

---
+++ firstwtpf/AddingWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System.Windows;
+
+namespace firstwtpf
+{
+    /// <summary>
+    /// Логика взаимодействия для AddingWindow.xaml
+    /// </summary>
+    public partial class AddingWindow : Window
+    {
+        public AddingWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Apply_Click(object sender, RoutedEventArgs e) {
+            if(PainterBox.Text != "" && TitleBox.Text != "" && DescriptionBox.Text != "") {
+                FirstWpfApp.MainWindow.fl = 0;
+                FirstWpfApp.MainWindow.NewArt(PainterBox.Text, TitleBox.Text, DescriptionBox.Text);
+                                   
+                this.Close();
+            } else {
+                MessageBox.Show("Fill in the fields","Empty");
+            }
+        }
+
+        private void Cancel_Click(object sender, RoutedEventArgs e) {
+            FirstWpfApp.MainWindow.fl = 1;
+            this.Close();
+        }
+    }
+}
--- firstwtpf/MainWindow.xaml.cs
+++ firstwtpf/MainWindow.xaml.cs
@@ -1,17 +1,12 @@
-﻿using System;
-using System.Windows;
-using System.Windows.Controls;
-
-using System.Collections.Generic;
+﻿using System.Windows;
 using System.Xml;
-using System.Xml.Serialization;
 
 namespace FirstWpfApp { 
     public partial class MainWindow : Window {
+        public static int fl = 0;           //bad
+
         public MainWindow() {
             InitializeComponent();
-
-            List<Picture> pictures = new List<Picture>();
 
             XmlDocument xd = new XmlDocument();
             xd.Load("XMLFile1.xml");
@@ -32,7 +27,6 @@ namespace FirstWpfApp {
                     }
                 }
                 artlist.Items.Add(pic);
-                pictures.Add(pic);
             }
         }
 
@@ -53,33 +47,82 @@ namespace FirstWpfApp {
                 this.Description = c;
             }
         }
+               
+        private void Add_Click(object sender, RoutedEventArgs e) {
+            firstwtpf.AddingWindow adding = new firstwtpf.AddingWindow {
+                Owner = this
+            };
+            adding.ShowDialog();
+            if (fl == 0) {
+                XmlDocument xd = new XmlDocument();
+                xd.Load("XMLFile1.xml");
+                XmlElement xRoot = xd.DocumentElement;
+                Picture pic = new Picture();
+                XmlNode xNode = xRoot.LastChild;
+                foreach (XmlNode childNode in xNode.ChildNodes) {
+                    if (childNode.Name == "Painter") {
+                        pic.Painter = childNode.InnerText;
+                    }
 
+                    if (childNode.Name == "Title") {
+                        pic.Title = childNode.InnerText;
+                    }
+
+                    if (childNode.Name == "Description") {
+                        pic.Description = childNode.InnerText;
+                    }
+                }
+                artlist.Items.Add(pic);
+            }
+        }
+        
         private void Exit_Click(object sender, RoutedEventArgs e) {
             this.Close();
         }
 
         private void Delete_Click(object sender, RoutedEventArgs e) {
-            int count = artlist.SelectedItems.Count;
-
             XmlDocument xd = new XmlDocument();
             xd.Load("XMLFile1.xml");
             XmlElement xRoot = xd.DocumentElement;
 
-            if (count > 0) {
-                while (artlist.SelectedItems.Count > 0) {
-                    dynamic selectedItem = artlist.SelectedItem;
-                    var p = selectedItem.Painter;
-                    XmlNode node = xd.SelectSingleNode("/pictures/picture[Painter='" + p + "']");
-                    node.ParentNode.RemoveChild(node);
+            while (artlist.SelectedItems.Count > 0) {
+                dynamic selectedItem = artlist.SelectedItem;
+                var p = selectedItem.Painter;
+                XmlNode node = xd.SelectSingleNode("/pictures/picture[Painter='" + p + "']");
+                node.ParentNode.RemoveChild(node);
+                xd.Save("XMLFile1.xml");
 
-                    xd.Save("XMLFile1.xml");
-
-                    artlist.Items.Remove(artlist.SelectedItem);
-                }
+                artlist.Items.Remove(artlist.SelectedItem);
             }
+
             if(artlist.Items.Count == 0) {
                 delete_button.IsEnabled = false;
             }
+        }
+
+        public static void NewArt(string p, string t, string d) {
+            XmlDocument xd = new XmlDocument();
+            xd.Load("XMLFile1.xml");
+            XmlElement xRoot = xd.DocumentElement;
+            XmlElement picElem = xd.CreateElement("picture");
+
+            XmlElement painElem = xd.CreateElement("Painter");
+            XmlElement titleElem = xd.CreateElement("Title");
+            XmlElement descElem = xd.CreateElement("Description");
+
+            XmlText painText = xd.CreateTextNode(p);
+            XmlText titleText = xd.CreateTextNode(t);
+            XmlText descText = xd.CreateTextNode(d);
+
+            painElem.AppendChild(painText);
+            titleElem.AppendChild(titleText);
+            descElem.AppendChild(descText);
+
+            picElem.AppendChild(painElem);
+            picElem.AppendChild(titleElem);
+            picElem.AppendChild(descElem);
+            xRoot.AppendChild(picElem);
+            xd.Save("XMLFile1.xml");           
         }
 
     }
